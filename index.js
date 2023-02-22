4f52ed89efbaea6be2da8b17671a8ef3e3ac500e
J <59581793+daechi73@users.noreply.github.com>
2023-02-22 16:00:56 -0500

    added: -index.html:innerAddBtn, outerAddBtn, and xBtn; -index.js:innerAddBtn.addEventListener complete

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const library = [];
 const content = document.getElementById("books");
+const outerAddBtn = document.getElementById("outerAddBtn");
+const innerAddBtn = document.getElementById("innerAddBtn");
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 }
 
 function addBook(book) {
@@ -13,7 +17,7 @@ function addBook(book) {
 }
 
 addBook(new Book("yo", "yoyo", "123", true));
-addBook(new Book("ja", "jaja", "456", true));
+addBook(new Book("ja", "jaja", "456", false));
 
 function showLibrary() {
   library.forEach((e) => {
@@ -21,9 +25,25 @@ function showLibrary() {
     <p> title: ${e.title}</p>
       <p> author: ${e.author}</p>
       <p> pages: ${e.pages}</p>
-      <p> read: ${e.read ? "read" : "not read"}
+      <p> read: ${e.read}
     </div>`;
   });
 }
 
+outerAddBtn.addEventListener("click", () => {
+  document.querySelector("form").removeAttribute("class");
+});
 showLibrary();
+
+innerAddBtn.addEventListener("click", () => {
+  const title = document.getElementById("title");
+  const author = document.getElementById("author");
+  const pages = document.getElementById("pages");
+  const read = document.getElementById("read");
+
+  content.innerHTML += `<div class="card"><p> Title: ${
+    title.value
+  }</p>  <p>Author: ${author.value}</p>  <p>Pages:${
+    pages.value
+  } </p> <p> Read: ${read.checked ? "Read" : "Unread"}</p> <div>`;
+});
